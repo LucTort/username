8561ae1696e743d4de9cd551824490c745cf4022
Lucas Torti <luctort@outlook.com>
2019-11-10 09:25:33 -0800

    Can no longer user upper case letters for username

--- username.sh
+++ username.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 # username.sh
 # Lucas Torti
-echo "The username must be between 3-12 characters, start with a lower case letter, and contain only letters, numbers, and underscores."
+echo "The username must be between 3-12 characters, start with a lower case letter, and contain only lower case letters, numbers, and underscores."
 echo "Enter a username: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][a-zA-z0-9_]{2,11}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "The username must be between 3-12 characters, start with a lower case letter, and contain only letters, numbers, and underscores."
+	echo "The username must be between 3-12 characters, start with a lower case letter, and contain only lower case letters, numbers, and underscores."
 	echo "Please enter a username: "
 	read USERNAME
 done
